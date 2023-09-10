ac449a6635c96400eedc74aa9d7147723f1eeaca
DanielMonteiroo <danieel.ios90@gmail.com>
2023-09-09 22:44:48 -0300

    Commit de arquivos da instrução prática 1

---
+++ meuprog1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+  string nome_do_usuario;
+
+  cout <<"Informe seu nome: ";
+
+  cin>>nome_do_usuario;  
+
+  cout <<"Bom dia "<<nome_do_usuario;
+    return 0;
+}
